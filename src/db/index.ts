a9b7b042f2b99dd0f1aeaf3c7f9f5d99a25a59d8
Audrow Nash <audrow@hey.com>
2022-01-04 07:39:05 -0800

    Run eslint on db
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,98 +9,98 @@ type RecipeSchema = {
 const recipes: RecipeSchema[] = [
   {
     id: 1,
-    name: "Eggs Benedict",
+    name: 'Eggs Benedict',
     ingredients: [
-      "eggs",
-      "butter",
-      "flour",
-      "salt",
-      "pepper",
-      "oil",
-      "vanilla",
+      'eggs',
+      'butter',
+      'flour',
+      'salt',
+      'pepper',
+      'oil',
+      'vanilla'
     ],
-    photo:"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400",
-    url: "https://www.bbcgoodfood.com/recipes/eggs-benedict",
+    photo: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400',
+    url: 'https://www.bbcgoodfood.com/recipes/eggs-benedict'
   },
   {
     id: 2,
-    name: "Cocoa Puffs",
+    name: 'Cocoa Puffs',
     ingredients: [
-      "cocoa",
-      "butter",
-      "flour",
-      "salt",
-      "pepper",
-      "oil",
-      "vanilla",
+      'cocoa',
+      'butter',
+      'flour',
+      'salt',
+      'pepper',
+      'oil',
+      'vanilla'
     ],
-    photo:"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400",
-    url: "https://www.bbcgoodfood.com/recipes/cocoa-puffs",
+    photo: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400',
+    url: 'https://www.bbcgoodfood.com/recipes/cocoa-puffs'
   },
   {
     id: 3,
-    name: "Chocolate Chip Cookies",
+    name: 'Chocolate Chip Cookies',
     ingredients: [
-      "chocolate",
-      "butter",
-      "flour",
-      "salt",
-      "pepper",
-      "oil",
-      "vanilla",
+      'chocolate',
+      'butter',
+      'flour',
+      'salt',
+      'pepper',
+      'oil',
+      'vanilla'
     ],
-    photo:"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400",
-    url: "https://www.bbcgoodfood.com/recipes/chocolate-chip-cookies",
+    photo: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400',
+    url: 'https://www.bbcgoodfood.com/recipes/chocolate-chip-cookies'
   },
   {
     id: 4,
-    name: "Cinnamon Rolls",
+    name: 'Cinnamon Rolls',
     ingredients: [
-      "cinnamon",
-      "butter",
-      "flour",
-      "salt",
-      "pepper",
-      "oil",
-      "vanilla",
+      'cinnamon',
+      'butter',
+      'flour',
+      'salt',
+      'pepper',
+      'oil',
+      'vanilla'
     ],
-    photo:"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400",
-    url: "https://www.bbcgoodfood.com/recipes/cinnamon-rolls",
-  },
-];
+    photo: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/smoked-salmon-scramble-croissants-9fc35c6.jpg?quality=90&webp=true&resize=440,400',
+    url: 'https://www.bbcgoodfood.com/recipes/cinnamon-rolls'
+  }
+]
 
-export function getRecipes() {
-  return recipes;
+export function getRecipes () {
+  return recipes
 }
 
-export function getIngredientsToRecipeIds() {
-  const ingredientsToRecipeId: { [ingredient: string]: number[] } = {};
+export function getIngredientsToRecipeIds () {
+  const ingredientsToRecipeId: { [ingredient: string]: number[] } = {}
 
   for (const recipe of recipes) {
     for (const ingredient of recipe.ingredients) {
       if (!(ingredient in ingredientsToRecipeId)) {
-        ingredientsToRecipeId[ingredient] = [];
+        ingredientsToRecipeId[ingredient] = []
       }
-      ingredientsToRecipeId[ingredient].push(recipe.id);
+      ingredientsToRecipeId[ingredient].push(recipe.id)
     }
   }
 
-  return ingredientsToRecipeId;
+  return ingredientsToRecipeId
 }
 
-export function getIngredients() {
-  return Object.keys(getIngredientsToRecipeIds());
+export function getIngredients () {
+  return Object.keys(getIngredientsToRecipeIds())
 }
 
-export function getRecipeIdsForIngredient(ingredient: string) {
-  return getIngredientsToRecipeIds()[ingredient];
+export function getRecipeIdsForIngredient (ingredient: string) {
+  return getIngredientsToRecipeIds()[ingredient]
 }
 
-export function getRecipeById(id: number) {
+export function getRecipeById (id: number) {
   for (const recipe of recipes) {
     if (recipe.id === id) {
-      return recipe;
+      return recipe
     }
   }
-  return null;
+  return null
 }
