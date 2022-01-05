3f58cf9eea3dc68d9a49ce709fcd09d453efafca
Audrow Nash <audrow@hey.com>
2022-01-05 08:43:07 -0800

    Integrate with recipes database
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-unused-vars */
 import * as React from 'react'
 
 import { BrowserRouter, Route, Routes, useSearchParams, useNavigate, Link } from 'react-router-dom'
 import seedrandom from 'seedrandom'
-import { Recipe } from './pages/Recipe'
-import { Recipes } from './pages/Recipes'
-import { PickRandom } from './pages/PickRandom'
 import { About } from './pages/About'
+import { getRecipes, getAllIngredients, getRecipesWithIngredients } from './db/index'
+import type { RecipeSchema } from './db/index'
 // import './App.css'
 
 export default function App () {
@@ -16,65 +13,15 @@ export default function App () {
       <Routes>
         <Route index element={<Picker />} />
         <Route path='/random' element={<Random />} />
-          {/* <Route index element={<PickRandom />} /> */}
-          {/* <Route path="/recipes" element={<Recipes />} />
-          <Route path="/recipe/:id" element={<Recipe />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<h1>Not Found</h1>} /> */}
-        {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
-const ingredients: string[] = [
-  'pasta',
-  'sauce',
-  'tomato',
-  'cheese',
-  'onion',
-  'garlic',
-  'basil',
-  'oregano',
-  'parmesan',
-  'mozzarella',
-  'pesto',
-  'apple',
-  'pear',
-  'banana',
-  'orange',
-  'lemon',
-  'pineapple',
-  'mango',
-  'watermelon',
-  'coconut',
-  'coconut milk'
-]
-
-const recipes: string[] = [
-  'pasta',
-  'pizza',
-  'soup',
-  'salad',
-  'cake',
-  'pie',
-  'sandwich',
-  'burger',
-  'steak',
-  'chicken'
-]
-
-function getIngredients () {
-  return ingredients
-}
-
-function getRecipes () {
-  return recipes
-}
-
 function Picker () {
-  const ingredients = getIngredients()
 
+  const [possibleIngredients, setPossibleIngredients] = React.useState<string[]>(getAllIngredients(getRecipes()))
   const [pickedIngredients, setPickedIngredients] = React.useState<string[]>([])
   const [searchText, setSearchText] = React.useState<string>('')
   const [filteredIngredients, setFilteredIngredients] = React.useState<string[]>([])
@@ -98,7 +45,7 @@ function Picker () {
     } else if (
       !formIngredient ||
       pickedIngredients.includes(formIngredient) ||
-      !ingredients.includes(formIngredient)
+      !possibleIngredients.includes(formIngredient)
     ) {
       return
     } else {
@@ -121,10 +68,12 @@ function Picker () {
     } else {
       setSearchParams({}, { replace: true })
     }
+    const remainingRecipes = getRecipesWithIngredients(pickedIngredients)
+    setPossibleIngredients(getAllIngredients(remainingRecipes))
   }, [pickedIngredients])
 
   React.useEffect(() => {
-    setFilteredIngredients(ingredients.filter(ingredient => {
+    setFilteredIngredients(possibleIngredients.filter(ingredient => {
       if (pickedIngredients.includes(ingredient)) {
         return false
       } else if (!searchText) {
@@ -135,7 +84,7 @@ function Picker () {
     }))
     setIsEnableAddButton(
       (filteredIngredients.length === 1) ||
-      ingredients.map(i => i.toLowerCase()).includes(searchText.toLowerCase())
+      possibleIngredients.map(i => i.toLowerCase()).includes(searchText.toLowerCase())
     )
     console.log()
   }, [searchText, pickedIngredients])
@@ -185,11 +134,12 @@ function Picker () {
 }
 
 function Random ({ recipesToPick = 3 }: { recipesToPick?: number }) {
+
   const [searchParams, setSearchParams] = useSearchParams()
   const searchIngredients = searchParams.get('ingredients')
   let seed = searchParams.get('seed') as string
   const [pickedIngredients, setPickedIngredients] = React.useState<string[]>([])
-  const [pickedRecipes, setPickedRecipes] = React.useState<string[]>([])
+  const [pickedRecipes, setPickedRecipes] = React.useState<RecipeSchema[]>([])
   const navigate = useNavigate()
 
   if (!seed) {
@@ -218,16 +168,17 @@ function Random ({ recipesToPick = 3 }: { recipesToPick?: number }) {
   }
 
   function pickRandomRecipes (randomSeed: string) {
-    const recipes = getRecipes()
+    const recipes = getRecipesWithIngredients(pickedIngredients)
 
     const random = seedrandom(randomSeed)
     const shuffledRecipes = [...recipes].sort(() => random() - 0.5)
-    let pickedRecipes: string[] = []
+    let pickedRecipes: RecipeSchema[] = []
     if (shuffledRecipes.length < recipesToPick) {
       pickedRecipes = shuffledRecipes
     } else {
       pickedRecipes = shuffledRecipes.slice(0, recipesToPick)
     }
+
     setPickedRecipes(pickedRecipes)
   }
 
@@ -245,7 +196,13 @@ function Random ({ recipesToPick = 3 }: { recipesToPick?: number }) {
       <ul>
         {pickedRecipes.map((recipe, index) => (
           <li key={index}>
-            {recipe}
+            <p>{recipe.name}</p>
+            <p>
+              {recipe.ingredients.join(', ')}
+            </p>
+            <p>
+              <a href={recipe.url}>Link</a>
+            </p>
           </li>
         ))}
       </ul>
