58385f18b89e22f69b788a7da195a7def593a558
mishmishmish <81111968+mishmishmish@users.noreply.github.com>
2022-01-16 21:46:41 -0800

    move nav to bottom of page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import Nav from './Nav'
 
-export default function Layout ({ children, isHideNav }: {children: any, isHideNav?: boolean}) {
+export default function Layout({ children, isHideNav }: { children: any, isHideNav?: boolean }) {
   return (
-        <div className="bg-yellow min-h-screen">
-            <div>
-                <h1 className="font-baloo text-3xl text-center p-5 text-navy">Random Recipe Wrangler!</h1>
-            </div>
-           <Link to="/about">
-                <button className="icon font-lobster text-white text-xl bg-pink border-3 border-navy rounded-full w-8 h-8 align-text-top absolute top-5 right-20">i</button>
-            </Link>
-           {!isHideNav && <Nav />}
-            {children}
+    <div className="bg-yellow min-h-screen">
+      <div>
+        <div>
+          <h1 className="font-baloo text-3xl text-center p-5 text-navy">Random Recipe Wrangler!</h1>
         </div>
+        <Link to="/about">
+          <button className="icon font-lobster text-white text-xl bg-pink border-3 border-navy 
+                rounded-full w-8 h-8 align-text-top absolute top-5 right-20">i</button>
+        </Link>
+        {children}
+        {!isHideNav && <Nav />}
+      </div>
+    </div>
   )
 }
