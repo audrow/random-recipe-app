ccaa661338a96a0121a31f26797abea30eddc01a
mishmishmish <81111968+mishmishmish@users.noreply.github.com>
2022-02-06 10:01:12 -0800

    hide about button in about page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,12 +8,14 @@ export default function Layout({ children, isHideNav, isHideAboutButton }: { chi
       <div>
           <h1 className=" font-baloo text-3xl text-center pt-10 text-navy">Random Recipe Wrangler!</h1>
         <div className='flex justify-center'>
+           {
+          !isHideAboutButton &&
           <div className='flex justify-end align-middle w-2/3'>
             <Link to="/about">
               <button className="icon font-lobster text-white text-xl bg-pink border-3 border-navy 
                 rounded-full w-8 h-8 align-text-top mb-3">i</button>
             </Link>
-          </div>
+          </div>}
         </div>
         {children}
         {!isHideNav && <Nav />}
