e310f820a4859bfaef42ca7ad8ac3cfb2d67ba0e
Audrow Nash <audrow@hey.com>
2022-02-09 09:48:56 -0800

    Fix going back to random page with no ingredients (#23)
    * Fix tab bug
    
    Signed-off-by: Audrow Nash <audrow@hey.com>
    
    * Improve search error message
    
    Signed-off-by: Audrow Nash <audrow@hey.com>
    
    * Fix random page with no ingredients
    
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/pages/Random.tsx
+++ src/pages/Random.tsx
@@ -33,9 +33,12 @@ function Random ({ recipesToPick = 3 }: { recipesToPick?: number }) {
   const [matchingRecipes, setMatchingRecipes] = React.useState<number>(0)
   const navigate = useNavigate()
 
-
   function resetSeed () {
     setSearchParams({course: searchCourse, ingredients: pickedIngredients.join('-') }, { replace: true })
+  }
+
+  if (!searchIngredients) {
+    navigate('/')
   }
 
   React.useEffect(() => {
